01e1d9e3689fb5284a2dc3731eb31a52933238b2
null-reference <guyfrompei@gmail.com>
2025-05-04 15:47:08 -0400

    unused import

--- src/components/FirstPitchCard.tsx
+++ src/components/FirstPitchCard.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
