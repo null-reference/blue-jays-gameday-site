8eba5668a92879f22112af93ae0938d9f5f6fb8c
null-reference <guyfrompei@gmail.com>
2025-05-23 16:07:19 -0400

    scale down logo

--- src/components/shared/BlueJaysLogo.tsx
+++ src/components/shared/BlueJaysLogo.tsx
@@ -7,10 +7,10 @@ interface BlueJaysLogoProps {
 const BlueJaysLogo = ({ className }: BlueJaysLogoProps) => {
   return (
     <Image
-      src="/logo-blue-jays-primary-1200x1035-trans.png"
+      src="/logo-blue-jays-primary-250x216-trans.png"
       alt="Blue Jays logo"
-      width={1200}
-      height={1035}
+      width={250}
+      height={216}
       className={className}
       style={{ width: '250px', height: 'auto' }}
     />
